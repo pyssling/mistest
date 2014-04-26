76502a3a45a1c6718c8de1ffa7b3eda2feddd89f
Nils Carlson <pyssling@ludd.ltu.se>
2014-04-26 22:36:27 +0200

    working with number

--- mistest/tap/parser.py
+++ mistest/tap/parser.py
@@ -18,7 +18,6 @@ class Parser:
         'OK',
         'NOT',
         'NUMBER',
-        'DESCRIPTION'
         )
 
     # Tokens
@@ -27,7 +26,7 @@ class Parser:
     t_NOT = r'not'
 
     def t_NUMBER(self, t):
-        r'\d+'
+        r"""\d+"""
         try:
             t.value = int(t.value)
         except ValueError:
@@ -36,7 +35,7 @@ class Parser:
         return t
 
 
-    t_DESCRIPTION = r'[^\d^#][^#]*'
+    #t_DESCRIPTION = r'[^#]*'
     #t_DIRECTIVE_SEP = r'#'
     #t_TODO = r'TODO'
     #t_SKIP = r'SKIP'
@@ -47,30 +46,28 @@ class Parser:
         print("Illegal character '%s'" % t.value[0])
         t.lexer.skip(1)
 
-#    def p_tap(self, p):
-#        """tap : ok"""
-#        p[0] = {
-#            "ok" : p[1],
-#            }
+    def p_tap(self, p):
+        """tap : ok number"""
+        print("tap", p[1], p[2])
+        p[0] = p[1]
 
     def p_ok(self, p):
         """ok : OK"""
-        print("ok")
-        p[0] = True
+        print("an ok")
+        p[0] = p[1]
 
     def p_not_ok(self, p):
         """ok : NOT OK"""
         print("An not ok")
-        p[0] = False
-
-    def p_number_or_empty(self, p):
-        """number_or_empty : NUMBER
-                           |"""
         p[0] = p[1]
 
-    def p_description_or_empty(self, p):
-        """description_or_empty : DESCRIPTION
-                                |"""
+    def p_number(self, p):
+        """number : NUMBER"""
+        p[0] = p[1]
+
+    def p_no_number(self, p):
+        """number : """
+        p[0] = 1
 
     def p_error(self, p):
         print("Syntax error")
@@ -90,6 +87,6 @@ class Parser:
             self.parser.parse(line)
 
 if __name__ == '__main__':
-    f = io.StringIO("ok\nnot ok\n")
+    f = io.StringIO("ok 1\nnot ok\n")
     p = Parser(f)
     p.parse()
